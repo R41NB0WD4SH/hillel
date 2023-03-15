17b7fcd15fc21752c53c647099d2c63a5d8c15d3
R41NB0WD4SH <116415348+R41NB0WD4SH@users.noreply.github.com>
2023-03-15 16:02:47 +0200

    Sum

---
+++ Lesson1_HW/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lesson1_HW
+{
+    class Programm
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            // GetSum();
+
+            Console.WriteLine(GetSum());
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+        }
+
+        public static int GetSum()
+        {
+            int result = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                result += i;
+            }
+
+            return result;
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+}
+
